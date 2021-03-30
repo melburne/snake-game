2c129faef2cf68b50701c74380071303069a24a5
melburne <melburne42@gmail.com>
2021-03-30 15:30:16 +0530

    Add logic to move snake

--- js/index.js
+++ js/index.js
@@ -8,9 +8,13 @@ let tileSize = canvas.width/tileCount - 2;
 let headX = 10;
 let headY = 10;
 
+let xVelocity = 0;
+let yVelocity = 0;
+
 
 function drawGame() {
   clearScreen();
+  changeSnakePosition();
   drawSnake();
   setTimeout(drawGame, 1000/speed);
 }
@@ -20,9 +24,43 @@ function clearScreen() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+
 function drawSnake() {
   ctx.fillStyle = 'orange';
   ctx.fillRect(headX*tileCount, headY*tileCount, tileSize, tileSize);
+}
+
+function changeSnakePosition() {
+  headX = headX + xVelocity;
+  headY = headY + yVelocity;
+}
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event) {
+  // up
+  if(event.keyCode == 38) {
+    yVelocity = -1;
+    xVelocity = 0;
+  }
+
+  // down
+  if(event.keyCode == 40) {
+      yVelocity = 1;
+      xVelocity = 0;
+  }
+
+  // left
+  if(event.keyCode == 37) {
+      yVelocity = 0;
+      xVelocity = -1;
+  }
+
+  // right
+  if(event.keyCode == 39) {
+      yVelocity = 0;
+      xVelocity = 1;
+  }
 }
 
 drawGame();
