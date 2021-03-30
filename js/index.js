458d502160778d69ace5ac476c45311539825c0f
melburne <melburne42@gmail.com>
2021-03-30 15:11:33 +0530

    Draw snake on canvas

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('snake-game');
+const ctx = canvas.getContext('2d');
+
+let speed = 7;
+
+let tileCount = 20;
+let tileSize = canvas.width/tileCount - 2;
+let headX = 10;
+let headY = 10;
+
+
+function drawGame() {
+  clearScreen();
+  drawSnake();
+  setTimeout(drawGame, 1000/speed);
+}
+
+function clearScreen() {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnake() {
+  ctx.fillStyle = 'orange';
+  ctx.fillRect(headX*tileCount, headY*tileCount, tileSize, tileSize);
+}
+
+drawGame();
