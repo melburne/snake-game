8ef3c4664d1984833d899bd5125be48213e73483
melburne <melburne42@gmail.com>
2021-03-31 15:41:17 +0530

    Add guard rails to restrict snake movement

--- js/index.js
+++ js/index.js
@@ -40,26 +40,38 @@ document.body.addEventListener('keydown', keyDown);
 function keyDown(event) {
   // up
   if(event.keyCode == 38) {
+    if (yVelocity == 1) {
+      return;
+    }
     yVelocity = -1;
     xVelocity = 0;
   }
 
   // down
   if(event.keyCode == 40) {
-      yVelocity = 1;
-      xVelocity = 0;
+    if (yVelocity == -1) {
+      return;
+    }
+    yVelocity = 1;
+    xVelocity = 0;
   }
 
   // left
   if(event.keyCode == 37) {
-      yVelocity = 0;
-      xVelocity = -1;
+    if (xVelocity == 1) {
+      return;
+    }
+    yVelocity = 0;
+    xVelocity = -1;
   }
 
   // right
   if(event.keyCode == 39) {
-      yVelocity = 0;
-      xVelocity = 1;
+    if (xVelocity == -1) {
+      return;
+    }
+    yVelocity = 0;
+    xVelocity = 1;
   }
 }
 
