346eae8d182220c27ae639e187bdc30863bdb0b7
melburne <melburne42@gmail.com>
2021-04-05 15:00:48 +0530

    Add comments and fix minor nits

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('snake-game');
 const ctx = canvas.getContext('2d');
 
+/**
+ * Defines the body of the snake.
+ */
 class SnakePart {
   constructor(x, y) {
     this.x = x;
@@ -8,25 +11,35 @@ class SnakePart {
   }
 }
 
+// determines the difficulty of the game
 let speed = 7;
-
-let tileCount = 20;
-let tileSize = canvas.width/tileCount - 2;
-let headX = 10;
-let headY = 10;
-const snakeParts = [];
-let tailLength = 2;
-
-let appleX = 5;
-let appleY = 5
-
 let xVelocity = 0;
 let yVelocity = 0;
 
+// tile dimensions
+let tileCount = 20;
+let tileSize = canvas.width/tileCount - 2;
+
+// head of the snake
+let headX = 10;
+let headY = 10;
+const snakeParts = [];
+
+// initial tail length
+let tailLength = 2;
+
+// initial apple position
+let appleX = 5;
+let appleY = 5
+
 let score = 0;
 
+// gulp sound played after snake eats apple
 const gulpSound = new Audio('audio/gulp.mp3');
 
+/**
+ * Handles drawing out the entire game and update intervals.
+ */
 function drawGame() {
   changeSnakePosition();
 
@@ -46,11 +59,19 @@ function drawGame() {
   setTimeout(drawGame, 1000/speed);
 }
 
+/**
+ * Moves snake in the direction determined by the velocity.
+ */
 function changeSnakePosition() {
   headX = headX + xVelocity;
   headY = headY + yVelocity;
 }
 
+/**
+ * Ends the game if the snake has collided with the wall or itself.
+ *
+ * @return true if the game is over and false otherwise
+ */
 function isGameOver() {
   let gameOver = false;
 
@@ -81,17 +102,23 @@ function isGameOver() {
   return gameOver;
 }
 
+/**
+ * Draws the canvas.
+ */
 function clearScreen() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+/**
+ * Checks if the snake has eaten with the apple and updates the score.
+ */
 function checkAppleCollision() {
   if(appleX == headX && appleY == headY) {
     let randomNumber = Math.floor(Math.random() * tileCount);
     appleX = randomNumber;
     appleY = randomNumber;
-    gulpSound.play();
+    gulpSound.play();  // may not work in Firefox
     tailLength++;
     score++;
 
@@ -102,36 +129,52 @@ function checkAppleCollision() {
   }
 }
 
+/**
+ * Draws the apple on the canvas.
+ */
 function drawApple() {
   ctx.fillStyle = 'red';
   ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
 }
 
+/**
+ * Draws the snake on the canvas.
+ */
 function drawSnake() {
+  // body of the snake
   ctx.fillStyle = 'green';
   for(let i=0; i<snakeParts.length; i++) {
     let part = snakeParts[i];
     ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
   }
 
+  // push new snake part where the head currently is
   snakeParts.push(new SnakePart(headX, headY));
   if(snakeParts.length > tailLength) {
+    // remove the first part of the body
     snakeParts.shift();
   }
 
+  // head of the snake
   ctx.fillStyle = 'orange';
   ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
 
+/**
+ * Draws the score on the upper right side of the canvas.
+ */
 function drawScore() {
   ctx.fillStyle = 'white';
   ctx.font = '12px Verdana';
   ctx.fillText('Score ' + score, canvas.width - 60, 15);
 }
 
-
+// check for key press events
 document.body.addEventListener('keydown', keyDown);
 
+/**
+ * Changes the velocity of the snake based on the arrow key pressed.
+ */
 function keyDown(event) {
   // up
   if(event.keyCode == 38) {
